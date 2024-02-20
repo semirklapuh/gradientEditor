e824a0e8e10adbb870e7dc8c8736473d2ef67a7e
Semir <semirklapuh@gmail.com>
2024-02-20 15:23:34 +0100

    git init

---
+++ gradientEditor/formTest.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace gradientEditor
+{
+    partial class formTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioBtnHex = new System.Windows.Forms.RadioButton();
+            this.radioBtnRgba = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.cbDirections = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 160);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(385, 153);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 362);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Result";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtResult);
+            this.panel1.Location = new System.Drawing.Point(13, 381);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 101);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResult.Location = new System.Drawing.Point(4, 38);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(764, 22);
+            this.txtResult.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Color list";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(461, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color format";
+            // 
+            // radioBtnHex
+            // 
+            this.radioBtnHex.AutoSize = true;
+            this.radioBtnHex.Location = new System.Drawing.Point(558, 268);
+            this.radioBtnHex.Name = "radioBtnHex";
+            this.radioBtnHex.Size = new System.Drawing.Size(55, 20);
+            this.radioBtnHex.TabIndex = 6;
+            this.radioBtnHex.TabStop = true;
+            this.radioBtnHex.Text = "HEX";
+            this.radioBtnHex.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnRgba
+            // 
+            this.radioBtnRgba.AutoSize = true;
+            this.radioBtnRgba.Location = new System.Drawing.Point(558, 296);
+            this.radioBtnRgba.Name = "radioBtnRgba";
+            this.radioBtnRgba.Size = new System.Drawing.Size(56, 20);
+            this.radioBtnRgba.TabIndex = 7;
+            this.radioBtnRgba.TabStop = true;
+            this.radioBtnRgba.Text = "rgba";
+            this.radioBtnRgba.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Type";
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(558, 164);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(121, 24);
+            this.cbType.TabIndex = 10;
+            this.cbType.SelectedIndexChanged += new System.EventHandler(this.cbType_SelectedIndexChanged);
+            // 
+            // cbDirections
+            // 
+            this.cbDirections.FormattingEnabled = true;
+            this.cbDirections.Location = new System.Drawing.Point(558, 208);
+            this.cbDirections.Name = "cbDirections";
+            this.cbDirections.Size = new System.Drawing.Size(121, 24);
+            this.cbDirections.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(464, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Direction";
+            // 
+            // formTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.cbDirections);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioBtnRgba);
+            this.Controls.Add(this.radioBtnHex);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "formTest";
+            this.Text = "formTest";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioBtnHex;
+        private System.Windows.Forms.RadioButton radioBtnRgba;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.ComboBox cbDirections;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ gradientEditor/formTest.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace gradientEditor
+{
+    public partial class formTest : Form
+    {
+        public formTest()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            InitializeComboBoxType();
+            InitializeComboBoxDirection();
+            InitializeRadioBtnColorFormat();
+        }
+
+        private void InitializeRadioBtnColorFormat()
+        {
+           radioBtnHex.Checked = true;
+        }
+
+        private void InitializeComboBoxDirection()
+        {
+            cbDirections.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void InitializeComboBoxType()
+        {
+            cbType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbType.Items.AddRange(new object[] { "linear", "radial" });
+            cbType.SelectedIndex = 0;
+        }
+
+        private void InitializeDataGridView()
+        {
+            dataGridView1.Columns.Add("Color", "Color");
+            dataGridView1.Columns.Add("ColorStop", "Color stop");
+
+            // Add a button column with a button in each cell
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.Name = "Color picker";
+            buttonColumn.Text = "Pick";
+            buttonColumn.UseColumnTextForButtonValue = true;
+            dataGridView1.Columns.Add(buttonColumn);
+
+            // Allow adding new rows
+            dataGridView1.AllowUserToAddRows = false;
+
+            // Handle button click event
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+
+            dataGridView1.RowHeadersVisible = true;
+            // Subscribe to the RowPostPaint event
+            dataGridView1.RowPostPaint += dataGridView1_RowPostPaint;
+
+
+            //// Get the text to insert (you can use any method to get the text)
+            //string newText = "Inserted Text";
+
+
+            // Add a new empty row
+            DataGridViewRow newRow = new DataGridViewRow();
+            newRow.CreateCells(dataGridView1);
+            dataGridView1.Rows.Add(newRow);
+
+            //Add result dummy value 
+            txtResult.TextAlign = HorizontalAlignment.Left;
+            //txtResult.TextAlign = Vertical;
+            txtResult.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the button column is clicked
+            if (e.ColumnIndex == dataGridView1.Columns["Color picker"].Index && e.RowIndex >= 0)
+            {
+                ColorDialog MyDialog = new ColorDialog();
+                // Keeps the user from selecting a custom color.
+                MyDialog.AllowFullOpen = false;
+                // Allows the user to get help. (The default is false.)
+                MyDialog.ShowHelp = true;
+                // Sets the initial color select to the current text color.
+                // Update the text box color if the user clicks OK 
+                string newText = "";
+                if (MyDialog.ShowDialog() == DialogResult.OK)
+                { 
+                    newText = "#" + (MyDialog.Color.ToArgb() & 0x00FFFFFF).ToString("X6");
+                }
+
+                // Insert text into the first column of the clicked row
+                dataGridView1.Rows[e.RowIndex].Cells["Color"].Value = newText;
+
+                //// Add a new empty row
+                //DataGridViewRow newRow = new DataGridViewRow();
+                //newRow.CreateCells(dataGridView1);
+                //dataGridView1.Rows.Add(newRow);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == dataGridView1.Rows.Count -1)
+            {
+                // Add a new empty row
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.CreateCells(dataGridView1);
+                dataGridView1.Rows.Add(newRow);
+            }
+
+            FormatResult(dataGridView1);
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            // Assuming dataGridView1 is the name of your DataGridView control
+            // Draw row numbers in the row headers
+            using (SolidBrush brush = new SolidBrush(dataGridView1.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, brush, e.RowBounds.Location.X + 10, e.RowBounds.Location.Y + 4);
+            }
+        }
+
+        private void FormatResult(DataGridView dataGridView)
+        {
+            string gradientResult = "";
+            string gradientType = "linear-gradient";
+            string gradientDirection = "to bottom";
+
+            gradientResult = gradientType + "(" + gradientDirection + ",";
+
+            foreach (DataGridViewRow row in dataGridView.Rows) {
+                if (row.Cells["Color"].Value != null && row.Cells["ColorStop"].Value != null) {
+                    var colorValue = row.Cells["Color"].Value.ToString();
+                    var colorStop = row.Cells["ColorStop"].Value.ToString();
+
+                    gradientResult += colorValue + " " + colorStop + ",";
+                }
+            }
+
+            gradientResult = gradientResult.Substring(0, gradientResult.Length - 1);
+            gradientResult += ")";
+
+            txtResult.Text = gradientResult;
+        }
+
+        private void cbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbType.SelectedItem.ToString() == "linear")
+            {
+                cbDirections.Items.Clear();
+                cbDirections.Items.AddRange(new object[] { "to bottom", "to top",
+                "to right", "to left", "to bottom right", "to bottom left", "to top right", "to top left" });
+                cbDirections.SelectedIndex = 0;
+            }
+            else
+            {
+                cbDirections.Items.Clear();
+                cbDirections.Items.AddRange(new object[] { "circle at center", "circle at center top","circle at top left",
+                "circle at top right", "circle at center bottom", "circle at left", "circle at right",
+                "circle at bottom right", "circle at bottom left"});
+                cbDirections.SelectedIndex = 0;
+            }
+        }
+    }
+}
